0e851b1212e953c874938195fb9648ea5e96843d
minquu <jeongmingyu@jeongmingyuui-MacBookPro.local>
2025-01-21 17:57:03 +0900

    테스트 코드 업로드

---
+++ DTO/Student.java
@@ -0,0 +1,19 @@
+package DTO;
+
+public class Student {
+    public String name;
+    public int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
